d46f0c0cda987b9fff0723ef588b8a913c7233b7
Yalu Cai <yc2454@cornell.edu>
2020-04-03 20:33:50 -0400

    basic structures of env

--- gym-futbol/__init__.py
+++ gym-futbol/__init__.py
@@ -8,6 +8,3 @@ register(
     id='futbol-extrahard-v0',
     entry_point='gym_futbol.envs:FutbolExtraHardEnv',
 )
-
-def my_cool_test_method():
-    print('It works!')
--- gym-futbol/env/futbol_env.py
+++ gym-futbol/env/futbol_env.py
@@ -1,17 +1,59 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import pandas as pd
+import numpy as np
 
 class FutbolEnv(gym.Env):
+  """Custom Environment that follows gym interface"""
   metadata = {'render.modes': ['human']}
 
-  def __init__(self):
+  def __init__(self, df):
+    # super(FutbolEnv, self).__init__()
+    # Define action and observation space
+
+    # df stands for data frame, containing the data that we might
+    # pass in during initiation.
+    ## From online code. May or may not use.
+    self.df = df
+
+    # data structure to contain all the actions
+    self.action_space = spaces.Discrete(4)
+
+    # data structure to contain observations the agent would make in one step
+    self.observation_space = spaces.Box(
+      low=0, high=1, shape=(6, 6), dtype=np.float16)
+
+  # observe the data at this step
+  def _next_observation(self):
     ...
+
+  def _take_action(self, action):
+    ...
+
+  # Execute one time step within the environment
   def step(self, action):
-    ...
+    self._take_action(action)
+
+    self.current_step += 1
+
+    # calculate reward
+    reward = 0
+
+    # figure out whether the process is done
+    done = False
+
+    obs = self._next_observation()
+
+    return obs, reward, done, {}
+
+  # Reset the state of the environment to an initial state
   def reset(self):
-    ...
-  def render(self, mode='human'):
-    ...
-  def close(self):
+    
+    # reset the state
+
+    return self._next_observation()
+
+  # Render the environment to the screen
+  def render(self, mode='human', close=False):
     ...
