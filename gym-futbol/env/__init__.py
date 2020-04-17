1acb3a1b2574cf4f94e7313ff09776693e336f4e
Yalu Cai <yc2454@cornell.edu>
2020-04-16 23:03:04 -0400

    naming error

--- gym-futbol/env/__init__.py
+++
@@ -1,2 +0,0 @@
-from gym_futbol.envs.futbol_env import FutbolEnv
-# from gym_football-non-us.envs.football-non-us_extrahard_env import FooExtraHardEnv
---
+++ gym_futbol/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_futbol.envs.futbol_env import FutbolEnv
+# from gym_futbol.envs.futbol_extrahard_env import FooExtraHardEnv
