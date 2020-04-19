bb8ad57ea4cfc85849b7f91b7d715f71f776c038
Yalu Cai <yc2454@cornell.edu>
2020-04-19 14:41:08 -0400

    change RUN action

--- gym_futbol/envs/futbol_env.py
+++ gym_futbol/envs/futbol_env.py
@@ -13,13 +13,19 @@ GOAL_LOWER = 304
 FIELD_LEN = 1000
 FIELD_WID = 600
 BALL_SPEED = 20
-PLARYER_SPEED = 9
+PLARYER_SPEED_W_BALL = 6
+PLARYER_SPEED_WO_BALL = 9
 GAME_TIME = 600
 
 def get_vec(coor1, coor2):
       vec = coor1[:2] - coor2[:2]
       vec_mag = math.sqrt(vec[0]**2 + vec[1]**2)
       return vec, vec_mag
+
+def move_by_vec(vec, vec_mag, loc):
+      loc[0] += loc[4] * (vec[0] / vec_mag)
+      loc[1] += loc[4] * (vec[1] / vec_mag)
+      loc[2:4] = vec
 
 class FutbolEnv(gym.Env):
 
@@ -31,7 +37,7 @@ class FutbolEnv(gym.Env):
             # the 5 values in the array represents: x coor, y coor, vector direction
             # sine, vector direction cosine, vector magnitude
             self.observation_space = spaces.Box(low=np.array([[0, 0, 0, 0, 0]] * 2), 
-                                                      high=np.array([[FIELD_LEN, FIELD_WID, 1.0, 1.0, PLARYER_SPEED],
+                                                      high=np.array([[FIELD_LEN, FIELD_WID, 1.0, 1.0, PLARYER_SPEED_W_BALL],
                                                       [FIELD_LEN, FIELD_WID, 1.0, 1.0, BALL_SPEED]]))
             # initial space
             self.init_space = spaces.Box(low=np.array([[FIELD_LEN/2, FIELD_WID/2, 0, 0, 0]] * 2), 
@@ -56,26 +62,26 @@ class FutbolEnv(gym.Env):
             # vector from ball to ai player
             b2a, b2a_mag = get_vec(self.ball[:2], self.ai[:2])
 
+            # vector from ball to opp
+            b2o, b2o_mag = get_vec(self.ball[:2], self.opp[:2])
+
             # vector from one of the goal tips to the ball
             prob = random.random()
             if prob > 0.5:
-                  g2b, g2b_mag = get_vec(np.array([FIELD_LEN, GOAL_LOWER]), self.ball[:2])
+                  rg2b, rg2b_mag = get_vec(np.array([FIELD_LEN, GOAL_LOWER]), self.ball[:2])
+                  lg2b, lg2b_mag = get_vec(np.array([0, GOAL_LOWER]), self.ball[:2])
             else:
-                  g2b, g2b_mag = get_vec(np.array([FIELD_LEN, GOAL_UPPER]), self.ball[:2])
+                  rg2b, rg2b_mag = get_vec(np.array([FIELD_LEN, GOAL_UPPER]), self.ball[:2])
+                  lg2b, lg2b_mag = get_vec(np.array([0, GOAL_LOWER]), self.ball[:2])
 
             if action_type == Action.SHOOT:
                   if self.ball_owner != BallOwner.AI:
                         pass
                   else:
-                        self.ball[4] = BALL_SPEED
-                        self.ball[0] += self.ball[4] * (g2b[0] / g2b_mag)
-                        self.ball[1] += self.ball[4] * (g2b[1] / g2b_mag)
-                        self.ball[2:4] = g2b
-                        o2b, o2b_mag = get_vec(self.opp[:2], self.ball[:2])
-                        self.opp[4] = PLARYER_SPEED
-                        self.opp[0] += self.opp[4] * (o2b[0] / o2b_mag)
-                        self.opp[1] += self.opp[4] * (o2b[1] / o2b_mag)
-                        self.opp[2:4] = o2b
+                        self.ball[4] = random.randint(BALL_SPEED - 20, BALL_SPEED + 10)
+                        move_by_vec(rg2b, rg2b_mag, self.ball)
+                        self.opp[4] = PLARYER_SPEED_WO_BALL
+                        move_by_vec(b2o, b2o_mag, self.opp)
 
             elif action_type == Action.TACKLE:
                   if ((b2a_mag > 0.5) or (self.ball_owner == BallOwner.AI)):
@@ -87,15 +93,24 @@ class FutbolEnv(gym.Env):
                               self.ball_owner = BallOwner.AI
 
             elif action_type == Action.RUN:
-                  self.ai[4] = PLARYER_SPEED
-                  self.ai[2:4] = b2a
-                  self.ai[0] += self.ai[4] * (b2a[0] / b2a_mag)
-                  self.ai[0] += self.ai[4] * (b2a[1] / b2a_mag)
+                  self.ai[4] = random.randint(PLARYER_SPEED_WO_BALL - 4, PLARYER_SPEED_WO_BALL + 4)
+                  self.opp[4] = random.randint(PLARYER_SPEED_WO_BALL - 4, PLARYER_SPEED_WO_BALL + 4)
                   o2a, o2a_mag = get_vec(self.opp[:2], self.ai[:2])
-                  self.opp[4] = PLARYER_SPEED
-                  self.opp[0] += self.opp[4] * (o2a[0] / o2a_mag)
-                  self.opp[1] += self.opp[4] * (o2a[1] / o2a_mag)
-                  self.opp[2:4] = o2a
+                  # if agent has the ball, agent run toward the goal, opp chase the 
+                  # agent
+                  if self.ball_owner == BallOwner.AI:
+                        move_by_vec(rg2b, rg2b_mag, self.ai)
+                        self.ball = self.ai
+                        move_by_vec(-o2a, o2a_mag, self.opp)
+                  # if opp has the ball, run towards opp
+                  elif self.ball_owner == BallOwner.OPP:
+                        move_by_vec(lg2b, lg2b_mag, self.opp)
+                        self.ball = self.opp
+                        move_by_vec(o2a, o2a_mag, self.ai)
+                  # if neither has the ball, run towards the ball
+                  else:
+                        move_by_vec(b2o, b2o_mag, self.opp)
+                        move_by_vec(b2a, b2a_mag, self.ai)
 
             else:
                   print('Unrecognized action %d' % action_type)
@@ -141,14 +156,10 @@ class FutbolEnv(gym.Env):
 
       # Reset the state of the environment to an initial state
       def reset(self):
-            # initial space
-            self.init_space = spaces.Box(low=np.array([[FIELD_LEN/2, FIELD_WID/2, 0, 0, 0]] * 2), 
-                                          high=np.array([[FIELD_LEN/2, FIELD_WID/2, 1.0, 1.0, 0],
-                                          [FIELD_LEN/2, FIELD_WID/2, 1.0, 1.0, 0]]))
             self.time = 0
             self.ball = np.array([FIELD_LEN/2, FIELD_WID/2, 0, 0, 0])
-            self.ai = np.array([FIELD_LEN/2, FIELD_WID/2, 0, 0, 0])
-            self.opp = np.array([FIELD_LEN/2, FIELD_WID/2, 0, 0, 0])
+            self.ai = np.array([FIELD_LEN/2 - 9, FIELD_WID/2, 0, 0, 0])
+            self.opp = np.array([FIELD_LEN/2 + 9, FIELD_WID/2, 0, 0, 0])
             self.ball_owner = BallOwner.NOONE
             return self._next_observation()
 
