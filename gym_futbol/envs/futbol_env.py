7fec58316491d51465328bf27730843cf97d1460
zhengxingXue <zx252cornell@gmail.com>
2020-05-11 22:58:59 -0400

    minor change

--- gym_futbol/envs/futbol_env.py
+++ gym_futbol/envs/futbol_env.py
@@ -135,7 +135,7 @@ class FutbolEnv(gym.Env):
                    game_time = GAME_TIME, player_speed = PLARYER_SPEED_W_BALL, 
                    shoot_speed = SHOOT_SPEED, Debug = False, pressure_range = PRESSURE_RANGE,
                    one_goal_end = False, action_as_int = True, only_reward_goal = False,
-                   random_opp = False):
+                   random_opp = True):
 
             # constants 
             self.length = length
